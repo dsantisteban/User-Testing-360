c75b2855233fdb4a56d2a1ad6fe2f07607239567
dsantisteban <diana.santisteban@hotmail.com>
2016-03-01 15:47:28 -0500

    responsive

--- js/menu-scroll.js
+++ js/menu-scroll.js
@@ -1,2 +1,12 @@
-
-
+$(document).ready(function(){
+    var altura = $('.navbar-inverse').offset().top;
+    
+    $(window).on('scroll', function(){
+        if ( $(window).scrollTop() > altura ){
+            $('.navbar-inverse').addClass('cambio');
+        } else {
+            $('.navbar-inverse').removeClass('cambio');
+        }
+    });
+ 
+});
